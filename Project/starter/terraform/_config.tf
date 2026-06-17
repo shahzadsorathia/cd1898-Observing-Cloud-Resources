8e44d21cddb52f32dc31c09b4a5d7c5f8caba340
Shahzad Sorathia <23331595+shahzadsorathia@users.noreply.github.com>
2026-06-16 20:43:48 -0500

    commit

--- Project/starter/terraform/_config.tf
+++ Project/starter/terraform/_config.tf
@@ -1,13 +1,13 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-sre-terraform-testing" # Replace it with your S3 bucket name
+     bucket = "shahzad-udacity-sre-course1-terraform" # Terraform state bucket
      key    = "terraform/terraform.tfstate"
-     region = "us-east-2"  # Update to your desired region
+     region = "us-east-1"  # set to the region where the S3 buckets exist
    }
  }
 
  provider "aws" {
-   region = "us-east-2"
+   region = "us-east-1"
 
    default_tags {
      tags = local.tags
